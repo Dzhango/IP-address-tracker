64a45f3274509b91f0ab9ff96631f7098ecea460
Dzhangir Bayandarov <dbayandarov@gmail.com>
2023-10-17 15:54:19 -0700

    refactor js

--- main.js
+++ main.js
@@ -1,37 +1,31 @@
 window.addEventListener("load", init);
-const map = L.map('map');
 
+const map = L.map('map');
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 function init() {
-    navigator.geolocation.getCurrentPosition((position) => {
-        updateMap(position.coords)
-    }, mapInit)
     initIpSearch();
 };
-
-
-function mapInit() {
-    const map = L.map('map').setView([51.505, -0.09], 13);
-
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-
-    L.marker([51.5, -0.09]).addTo(map)
-        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-        .openPopup();
-}
 
 function initIpSearch() {
     const inputIP = document.querySelector("#ip-input");
     const submitBtn = document.querySelector("#submit");
     submitBtn.addEventListener('click', (e) => {
-        fetchIP(inputIP.value);
-    })
-    fetchIP("");
+        fetchIPMap(inputIP.value);
+        fetchIpData(inputIP.value);
+    });
+    window.addEventListener('keyup', (e) => {
+        if (e.keyCode === 13) {
+            fetchIPMap(inputIP.value);
+            fetchIpData(inputIP.value);
+        }
+    });
+    // check is API flag for client's IP
+    fetchIPMap("check");
+    fetchIpData("");
+
 }
 
 function updateDOM(json) {
@@ -42,7 +36,6 @@ function updateDOM(json) {
     pTagArray[1].innerText = `${json.location.region}, ${json.location.country}`;
     pTagArray[2].innerText = `UTC ${json.location.timezone}`;
     pTagArray[3].innerText = json.isp;
-
 }
 
 function updateMap(coordinates) {
@@ -52,31 +45,26 @@ function updateMap(coordinates) {
         .openPopup();
 }
 
+async function fetchIPMap(ip) {
+    const url = `http://api.ipstack.com/${ip}?access_key=f71aa2c29208c9164be4bd0d782a3e18`;
 
-async function fetchIP(ip) {
-    try {
-        const response = await fetch(
-            `http://api.ipstack.com/${ip}?access_key=f71aa2c29208c9164be4bd0d782a3e18`);
-        const json = await response.json();
-        if (response.status === 200) {
-            updateMap(json);
-        } else {
-            console.error(`Responded with status code: ${response.status}`)
-        }
-    } catch (error) {
-        console.log(error)
+    const response = await fetch(url);
+    const json = await response.json();
+    if (response.status === 200) {
+        updateMap(json);
+    } else {
+        console.error(`Responded with status code: ${response.status}`)
     }
+}
 
-    try {
-        const response = await fetch(
-            `https://geo.ipify.org/api/v2/country?apiKey=at_Pje4dhNLiddNfP5Cr2rZpaHVbRvEz&ipAddress=${ip}`);
-        const json = await response.json();
-        if (response.status === 200) {
-            updateDOM(json);
-        } else {
-            console.error(`Responded with status code: ${response.status}`)
-        }
-    } catch (error) {
-        console.log(error)
+async function fetchIpData(ip) {
+    const url = `https://geo.ipify.org/api/v2/country?apiKey=at_Pje4dhNLiddNfP5Cr2rZpaHVbRvEz&ipAddress=${ip}`;
+    const response = await fetch(url);
+    const json = await response.json();
+    if (response.status === 200) {
+        updateDOM(json);
+
+    } else {
+        console.error(`Responded with status code: ${response.status}`)
     }
 }
